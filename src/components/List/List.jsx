674c3fbe5e9241a324cb75bc99c4cd66713bee81
liuxp <liuxp@lenovocloud.com>
2020-12-30 14:30:07 +0800

    init test

---
+++ src/components/List/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const List = (items, EidtFunction, DeleteFunction) => {
+    return (
+        <ul>
+            <li>DDDD</li>
+        </ul>
+    );
+}
+
+export default List;
---
+++ src/container/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import List from '../../components/List/List'
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <List></List>
+            </div>
+        )
+    }
+}
+
+export default Home;
